34c438cc45185a26692c70bfb3493942d19fa665
Nicolas Bastien <nbastien@prestaconcept.net>
2013-11-04 11:58:03 +0100

    Bundle initialisation

---
+++ DependencyInjection/PrestaSonataAdminExtendedExtension.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * This file is part of the PrestaSonataAdminExtendedBundle
+ *
+ * (c) PrestaConcept <www.prestaconcept.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Presta\SonataAdminExtendedBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Loader;
+
+/**
+ * @author Nicolas Bastien <nbastien@prestaconcept.net>
+ */
+class PrestaSonataAdminExtendedExtension extends Extension
+{
+    /**
+     * {@inheritDoc}
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $configuration = new Configuration();
+        $this->processConfiguration($configuration, $configs);
+    }
+}
---
+++ PrestaSonataAdminExtendedBundle.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * This file is part of the PrestaSonataAdminExtendedBundle
+ *
+ * (c) PrestaConcept <www.prestaconcept.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+namespace Presta\SonataAdminExtendedBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * @author Nicolas Bastien <nbastien@prestaconcept.net>
+ */
+class PrestaSonataAdminExtendedBundle extends Bundle
+{
+}
